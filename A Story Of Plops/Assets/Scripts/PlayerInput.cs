b8b1cdef103da127aac4cfddbcaf6d87551382ce
Bernardo <behornerl@gmail.com>
2020-03-15 22:00:08 -0300

    Criando script para receber os inputs do jogador

---
+++ A Story Of Plops/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float vertical, horizontal;
+    [SerializeField] private CharacterMovement characterMovement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        characterMovement.MoveInDirection(horizontal, vertical);
+    }
+}
