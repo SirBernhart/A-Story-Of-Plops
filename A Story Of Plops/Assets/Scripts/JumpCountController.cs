ba73837c013ad03b26d111b87d032b2dde5f409f
Bernardo <behornerl@gmail.com>
2020-03-15 23:49:27 -0300

    Criando script para gerenciar o número de pulos do jogador

---
+++ A Story Of Plops/Assets/Scripts/JumpCountController.cs
@@ -0,0 +1,38 @@
+﻿public class JumpCountController
+{
+    private int maxJumpCount = 3;
+    private int jumpCount;
+
+    public void AddJumpCount()
+    {
+        if(jumpCount < maxJumpCount)
+        {
+            ++jumpCount;
+        }
+    }
+
+    public void ResetJumpCount()
+    {
+        jumpCount = 0;
+    }
+
+    public bool CheckCanJump()
+    {
+        return jumpCount < maxJumpCount;
+    }
+
+    public void IncreaseMaxJumpCount()
+    {
+        ++maxJumpCount;
+    }
+
+    public int GetMaxJumpCount()
+    {
+        return maxJumpCount;
+    }
+    
+    public int GetJumpCount()
+    {
+        return jumpCount;
+    }
+}
