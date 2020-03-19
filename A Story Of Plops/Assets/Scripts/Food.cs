86d854897739852e146d5fd76656d2e8032a7cd1
Bernardo <behornerl@gmail.com>
2020-03-18 23:52:08 -0300

    Criando script de comida

---
+++ A Story Of Plops/Assets/Scripts/Food.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    private int jumpsToIncrease;
+
+    // "Eat"
+    public int Eat()
+    {
+        Destroy(gameObject, 0.2f);
+        return jumpsToIncrease;
+    }
+}
