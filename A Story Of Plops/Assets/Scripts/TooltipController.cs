905ecd54894062ab1582b80e6dd513636d480cb6
Bernardo <behornerl@gmail.com>
2020-03-20 10:44:44 -0300

    Adicionando script de controle de tooltip

---
+++ A Story Of Plops/Assets/Scripts/TooltipController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipController : MonoBehaviour
+{
+    [SerializeField] GameObject eatTooltip;
+
+    public void ShowEatTooltip(bool shouldShow)
+    {
+        if (shouldShow)
+        {
+            eatTooltip.SetActive(true);
+        }
+        else
+        {
+            eatTooltip.SetActive(false);
+        }
+    }
+}
