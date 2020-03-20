25a150a1f51c0b046273d90ee5692d61480cdf4c
Bernardo <behornerl@gmail.com>
2020-03-20 00:14:17 -0300

    Adicionando script que mostra/esconde o mouse

---
+++ A Story Of Plops/Assets/Scripts/MouseHidder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHidder : MonoBehaviour
+{
+    private void Start()
+    {
+        ShowCursor(false);
+    }
+
+    public void ShowCursor(bool shouldShow)
+    {
+        if (shouldShow)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
