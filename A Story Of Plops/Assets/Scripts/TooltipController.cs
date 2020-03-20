ae09ea7b3942d9edb30e24bddaa81011925e7996
Bernardo <behornerl@gmail.com>
2020-03-20 13:08:30 -0300

    Placeholder de funcionalidade de tooltip

--- A Story Of Plops/Assets/Scripts/TooltipController.cs
+++ A Story Of Plops/Assets/Scripts/TooltipController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TooltipController : MonoBehaviour
 {
     [SerializeField] GameObject eatTooltip;
+    [SerializeField] GameObject controlsTooltip;
 
     public void ShowEatTooltip(bool shouldShow)
     {
@@ -16,5 +17,22 @@ public class TooltipController : MonoBehaviour
         {
             eatTooltip.SetActive(false);
         }
+    }
+
+    public void ShowMovementControls(bool shouldShow)
+    {
+        if (shouldShow)
+        {
+            controlsTooltip.SetActive(true);
+        }
+        else
+        {
+            controlsTooltip.SetActive(true);
+        }
+    }
+
+    private void TimerToHide()
+    {
+        ShowMovementControls(false);
     }
 }
