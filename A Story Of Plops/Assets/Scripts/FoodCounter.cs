d56e078b035bf60cc2d42d6d63af5ac88aaac3ce
Bernardo <behornerl@gmail.com>
2020-03-20 00:34:42 -0300

    Adicionando script de controle da quantidade de comidas disponíveis

---
+++ A Story Of Plops/Assets/Scripts/FoodCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodCounter : MonoBehaviour
+{
+    [SerializeField] private Text availableFoodDisplay;
+    [SerializeField] private Transform foodParent;
+    private int totalFood;
+    private int foodEaten;
+
+    private void Start()
+    {
+        totalFood = foodParent.childCount;
+        UpdateFoodDisplay();
+    }
+
+    public void IncreaseFoodEaten()
+    {
+        ++foodEaten;
+        UpdateFoodDisplay();
+    }
+
+    private void UpdateFoodDisplay()
+    {
+        availableFoodDisplay.text = foodEaten + " / " + totalFood;
+    }
+}
